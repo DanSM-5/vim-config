edf6488d8ab0097034d9b914657763d1441932b7
daniel <eduardo.daniel.s.m@gmail.com>
2024-08-06 16:35:25 -0400

    fix: fix config setup for bashls for termux

--- lua/lsp-servers/termux.lua
+++ lua/lsp-servers/termux.lua
@@ -14,39 +14,22 @@ return {
       vim.api.nvim_create_autocmd('BufEnter', {
         callback = function()
           vim.lsp.start({
-            {
-              cmd = { 'bash-language-server', 'start' },
-              settings = {
-                bashIde = {
-                  -- Glob pattern for finding and parsing shell script files in the workspace.
-                  -- Used by the background analysis features across files.
+            cmd = { 'bash-language-server', 'start' },
+            settings = {
+              bashIde = {
+                -- Glob pattern for finding and parsing shell script files in the workspace.
+                -- Used by the background analysis features across files.
 
-                  -- Prevent recursive scanning which will cause issues when opening a file
-                  -- directly in the home directory (e.g. ~/foo.sh).
-                  --
-                  -- Default upstream pattern is "**/*@(.sh|.inc|.bash|.command)".
-                  globPattern = vim.env.GLOB_PATTERN or bashls_opts.settings.bashIde.globPattern,
-                },
-              },
-              filetypes = { 'sh' },
-              root_dir = util.find_git_ancestor,
-              single_file_support = true,
-            },
-            docs = {
-              description = [[
-https://github.com/bash-lsp/bash-language-server
-
-`bash-language-server` can be installed via `npm`:
-```sh
-npm i -g bash-language-server
-```
-
-Language server for bash, written using tree sitter in typescript.
-]],
-              default_config = {
-                root_dir = [[util.find_git_ancestor]],
+                -- Prevent recursive scanning which will cause issues when opening a file
+                -- directly in the home directory (e.g. ~/foo.sh).
+                --
+                -- Default upstream pattern is "**/*@(.sh|.inc|.bash|.command)".
+                globPattern = vim.env.GLOB_PATTERN or bashls_opts.settings.bashIde.globPattern,
               },
             },
+            filetypes = { 'sh' },
+            root_dir = util.find_git_ancestor,
+            single_file_support = true,
           })
         end
       })
