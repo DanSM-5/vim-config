48ea9d8326072d36b5d7cc25c457373f612895d9
daniel <eduardo.daniel.s.m@gmail.com>
2024-08-18 12:27:14 -0400

    feat: Silence neotree commands

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -10,27 +10,27 @@ return {
   config = function ()
     vim.keymap.set(
       'n', '<leader>ve', ':Neotree filesystem toggle reveal_force_cwd position=right<cr>',
-      { desc = 'NeoTree: Open current directory in drawer' }
+      { desc = 'NeoTree: Open current directory in drawer', silent = true }
     )
     vim.keymap.set(
-      'n', '<leader>vE', ':Neotree filesystem show toggle reveal_force_cwd position=right<cr>',
-      { desc = 'NeoTree: Open current directory in drawer but preserve focus on current buffer' }
+      'n', '<leader>ne', ':Neotree filesystem show toggle reveal_force_cwd position=right<cr>',
+      { desc = 'NeoTree: Open current directory in drawer but preserve focus on current buffer', silent = true }
     )
     vim.keymap.set(
       'n', '<leader>vc', ':Neotree filesystem show toggle dir=$user_conf_path position=right<cr>',
-      { desc = 'NeoTree: Open user config directory' }
+      { desc = 'NeoTree: Open user config directory', silent = true }
     )
     vim.keymap.set(
       'n', '<leader>vs', ':Neotree filesystem show toggle dir=$user_scripts_path position=right<cr>',
-      { desc = 'NeoTree: Open user scripts directory' }
+      { desc = 'NeoTree: Open user scripts directory', silent = true }
     )
     vim.keymap.set(
       'n', '<leader>vv', ':Neotree filesystem show toggle dir=$HOME/vim-config position=right<cr>',
-      { desc = 'NeoTree: Open vim config directory' }
+      { desc = 'NeoTree: Open vim config directory', silent = true }
     )
     vim.keymap.set(
       'n', '<leader>vp', ':Neotree filesystem show toggle dir=$HOME/projects position=right<cr>',
-      { desc = 'NeoTree: Open projects directory' }
+      { desc = 'NeoTree: Open projects directory', silent = true }
     )
 
     -- Use same highlights for NormalNC
