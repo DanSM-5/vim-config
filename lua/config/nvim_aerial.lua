6bd3fcb06e50dbbada5f22f1e187ea6162dd5554
daniel <eduardo.daniel.s.m@gmail.com>
2024-10-18 16:07:17 -0400

    feat: Add config file for aerial.nvim

---
+++ lua/config/nvim_aerial.lua
@@ -0,0 +1,13 @@
+return {
+  setup = function()
+    require('aerial').setup({
+      on_attach = function(bufnr)
+        vim.keymap.set('n', '[a', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+        vim.keymap.set('n', ']a', '<cmd>AerialNext<CR>', { buffer = bufnr })
+      end,
+    })
+
+    vim.keymap.set('n', '<leader>ta', '<cmd>AerialToggle!<cr>', { desc = '[Aerial] Toggle window', noremap = true })
+    vim.keymap.set('n', '<leader>fa', '<cmd>call aerial#fzf()<cr>', { desc = '[Aerial] Aerial fzf selector', noremap = true })
+  end,
+}
