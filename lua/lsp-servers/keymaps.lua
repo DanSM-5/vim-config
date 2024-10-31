a02a575627b8822d50f974537ff2039095f0902a
daniel <eduardo.daniel.s.m@gmail.com>
2024-10-31 00:05:19 -0400

    feat: Add command "InspectLspClient"

--- lua/lsp-servers/keymaps.lua
+++ lua/lsp-servers/keymaps.lua
@@ -2,6 +2,81 @@ local exclude_filetypes = {
   'help',
 }
 
+---Creates a float window with lsp information
+---@param client_name string
+local render_lsp_info = function(client_name)
+  if client_name then
+    local client = vim.lsp.get_clients({ name = client_name })
+
+    if #client == 0 then
+      vim.notify('No active LSP clients found with this name: ' .. client_name, vim.log.levels.WARN)
+      return
+    end
+
+    -- Create a temporary buffer to show the configuration
+    local buf = vim.api.nvim_create_buf(false, true)
+    local win = vim.api.nvim_open_win(buf, true, {
+      relative = 'editor',
+      width = math.floor(vim.o.columns * 0.75),
+      height = math.floor(vim.o.lines * 0.90),
+      col = math.floor(vim.o.columns * 0.125),
+      row = math.floor(vim.o.lines * 0.05),
+      style = 'minimal',
+      border = 'rounded',
+      title = ' ' .. (client_name:gsub('^%l', string.upper)) .. ': LSP Configuration ',
+      title_pos = 'center',
+    })
+
+    local lines = {}
+    for i, this_client in ipairs(client) do
+      if i > 1 then
+        table.insert(lines, string.rep('-', 80))
+      end
+      table.insert(lines, 'Client: ' .. this_client.name)
+      table.insert(lines, 'ID: ' .. this_client.id)
+      table.insert(lines, '')
+      table.insert(lines, 'Configuration:')
+
+      local config_lines = vim.split(vim.inspect(this_client.config), '\n')
+      vim.list_extend(lines, config_lines)
+    end
+
+    -- Set the lines in the buffer
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+
+    -- Set buffer options
+    vim.bo[buf].modifiable = false
+    vim.bo[buf].filetype = 'lua'
+    vim.bo[buf].bh = 'delete'
+
+    vim.api.nvim_buf_set_keymap(buf, 'n', 'q', ':q<CR>', { noremap = true, silent = true })
+    vim.api.nvim_buf_set_keymap(buf, 'n', '<Esc>', ':q<CR>', { noremap = true, silent = true })
+  end
+end
+
+local select_client_fzf = function()
+  require('shared.fzf').select_buffer_lsp(function(selected)
+    -- NOTE: sink function always return an empty string on first position
+    if #selected < 2 then
+      return
+    end
+    vim.print(selected)
+    render_lsp_info(selected[2])
+  end)
+end
+
+---Options passed to command
+---@param opts { fargs: string[]; bang: boolean }
+local function inspect_lsp_client(opts)
+  if opts.bang then
+    select_client_fzf()
+  elseif opts.fargs[1] ~= nil then
+    render_lsp_info(opts.fargs[1])
+  else
+    vim.ui.input({ prompt = 'Enter LSP Client name: ' }, render_lsp_info)
+  end
+end
+
 return {
   setup = function()
     -- Global mappings
@@ -22,7 +97,6 @@ return {
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('UserLspConfig', {}),
       callback = function(ev)
-
         -- Prevent adding keymaps if we are excluding specific file types such as help
         -- Other options could be trying to set negative pattern in filetype
         -- See: https://groups.google.com/g/vim_use/c/I_ssfzT8FD8
@@ -102,12 +176,14 @@ return {
         -- Remap to equivalent with [ motion
         vim.keymap.set('n', '<space>ne', ']e', { desc = 'LSP: Go to next error', remap = true })
         vim.keymap.set('n', '<space>nw', ']w', { desc = 'LSP: Go to next warning', remap = true })
-        vim.keymap.set('n', '<space>ni', ']h', { desc = 'LSP: Go to next hint', remap = true})
+        vim.keymap.set('n', '<space>ni', ']h', { desc = 'LSP: Go to next hint', remap = true })
 
         vim.keymap.set('n', '<space>nE', '[e', { desc = 'LSP: Go to previous error', remap = true })
         vim.keymap.set('n', '<space>nW', '[w', { desc = 'LSP: Go to previous warning', remap = true })
         vim.keymap.set('n', '<space>nI', '[h', { desc = 'LSP: Go to previous hint', remap = true })
       end,
     })
+
+    vim.api.nvim_create_user_command('InspectLspClient', inspect_lsp_client, { nargs = '?', bang = true })
   end,
 }
