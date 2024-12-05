3b3d7419db85513a6d1a887043d14db9bc2ab0ba
daniel <eduardo.daniel.s.m@gmail.com>
2024-12-04 22:19:06 -0500

    feat(lsp-config): Add settings for jsonls server

--- lua/lsp-servers/config.lua
+++ lua/lsp-servers/config.lua
@@ -45,6 +45,21 @@ local configs = {
     -- Prevent loading for all lsps
     filetypes = { 'go', 'c' },
   },
+  jsonls = {
+    -- lazyload schemastore when needed
+    on_new_config = function(new_config)
+      new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+      vim.list_extend(new_config.settings.json.schemas, require('schemastore').json.schemas())
+    end,
+    settings = {
+      json = {
+        format = {
+          enable = true,
+        },
+        validate = { enable = true },
+      },
+    },
+  }
 }
 
 return {
