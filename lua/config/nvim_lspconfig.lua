f73aa41767b8c70dfbb85d996b91ebb71e3ccbc9
daniel <daniel@daniel-predator.env>
2025-01-01 10:56:55 -0500

    feat(lsp): Add code to scroll hover window for lsp

--- lua/config/nvim_lspconfig.lua
+++ lua/config/nvim_lspconfig.lua
@@ -16,6 +16,20 @@ return {
     --     vim.ui.open(vim.fn.expand("<cfile>"))
     --   end
     -- end, { desc = "Open next link", silent = true })
+
+    -- Scroll lsp window without needing to enter it
+    -- TODO: Find a way to only enable keymap when preview window is visible
+    --
+    -- local function scrollLspWin(lines)
+    --   local winid = vim.b.lsp_floating_preview --> stores id of last `vim.lsp`-generated win
+    --   if not winid or not vim.api.nvim_win_is_valid(winid) then return end
+    --   vim.api.nvim_win_call(winid, function()
+    --     local topline = vim.fn.winsaveview().topline
+    --     vim.fn.winrestview({ topline = topline + lines })
+    --   end)
+    -- end
+    -- vim.keymap.set('n', '<PageDown>', function() scrollLspWin(5) end, { desc = '↓ Scroll LSP window' })
+    -- vim.keymap.set('n', '<PageUp>', function() scrollLspWin(-5) end, { desc = '↑ Scroll LSP window' })
   end,
 }
 
