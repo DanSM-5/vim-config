c306424653d16e999665429a88e70c916a345526
daniel <eduardo.daniel.s.m@gmail.com>
2025-01-19 11:56:32 -0500

    refactor(mpls): Improve logic for lsp start

--- lua/config/nvim_mpls.lua
+++ lua/config/nvim_mpls.lua
@@ -43,32 +43,37 @@ live preview of markdown files in your browser while you edit them in your favor
       -- Start client if current buffer is markdown
       local bufnr = vim.api.nvim_get_current_buf()
       local curr_buff_ft = vim.api.nvim_get_option_value('filetype', { buf = bufnr })
+
+      -- Continue only if in a markdown buffer
+      if curr_buff_ft ~= 'markdown' then
+        return
+      end
+
       local base_config = require('lsp-servers.config').get_config('mpls') or {}
-      if curr_buff_ft == 'markdown' then
-        -- Try to gess root dir
-        local root_dir = vim.fs.dirname(vim.fs.find('.git', { path = vim.fn.expand('%:p:h'), upward = true })[1]) or vim.fn.expand('%:p:h')
-        -- Start lsp on buffer
-        local client_id = vim.lsp.start(
-          vim.tbl_deep_extend('force', {
-            cmd = { 'mpls', '--dark-mode', '--enable-emoji' },
-            filetypes = { 'markdown' },
-            single_file_support = true,
-            root_dir = root_dir,
-            settings = {},
-            on_attach = require('lsp-servers.keymaps').set_lsp_keys
-          }, base_config)
+      -- Try to gess root dir
+      local root_dir = vim.fs.dirname(vim.fs.find('.git', { path = vim.fn.expand('%:p:h'), upward = true })[1]) or vim.fn.expand('%:p:h')
+      -- Start lsp on buffer
+      local client_id = vim.lsp.start(
+        vim.tbl_deep_extend('force', {
+          cmd = { 'mpls', '--dark-mode', '--enable-emoji' },
+          filetypes = { 'markdown' },
+          single_file_support = true,
+          root_dir = root_dir,
+          settings = {},
+          on_attach = require('lsp-servers.keymaps').set_lsp_keys
+        }, base_config)
         , {
-            bufnr = bufnr,
-            silent = false,
-            reuse_client = function () return false end
+          bufnr = bufnr,
+          silent = false,
+          reuse_client = function () return false end
         })
 
-        -- Attach to client
-        if client_id ~= nil then
-          vim.lsp.buf_attach_client(bufnr, client_id)
-          vim.notify('[MPLS] bufnr: '..bufnr..' client: '..client_id, vim.log.levels.INFO)
-        end
+      -- Attach to client
+      if client_id ~= nil then
+        vim.lsp.buf_attach_client(bufnr, client_id)
+        vim.notify('[MPLS] bufnr: '..bufnr..' client: '..client_id, vim.log.levels.INFO)
       end
     end, { desc = '[Lsp] Start mpls lsp server', bar = true, bang = true, nargs = 0 })
   end
 }
+
