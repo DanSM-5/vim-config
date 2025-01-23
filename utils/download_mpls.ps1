6d4339e967888b42178d610b05d270a77a779c49
daniel <eduardo.daniel.s.m@gmail.com>
2025-01-22 23:45:27 -0500

    fix(download_mpls): fix scripts

--- utils/download_mpls.ps1
+++ utils/download_mpls.ps1
@@ -1,6 +1,7 @@
 #!/usr/bin/env pwsh
 
 # NOTE: Curl should be present since windows 10
+if (Test-Path Alias:curl) { Remove-Item Alias:curl }
 
 if ($IsWindows -or ($env:OS -eq 'Windows_NT')) {
   $platform = 'windows'
@@ -34,18 +35,23 @@ if (-Not $string_url) {
 $tmp_dir = if ($platform -eq 'windows') { $env:TEMP } else { '/tmp' }
 $download_file = "mpls_download.${ext}"
 $download_path = "${tmp_dir}${dirsep}${download_file}"
-$extract_dir = "${$tmp_dir}${dirsep}mpls_extracted"
+$extract_dir = "${tmp_dir}${dirsep}mpls_extracted"
 
 # It will always override unless you use `--no-clobber` flag
 curl -sL "$string_url" -o "$download_path"
 
+if (Test-Path -Path $extract_dir -PathType Container -ErrorAction SilentlyContinue) {
+  Remove-Item -Force -Recurse -Path $extract_dir
+}
+
+New-Item -ItemType Directory -Path "$extract_dir" -ErrorAction SilentlyContinue
+
 if ($platform -eq 'windows') {
-  Expand-Archive -Path $download_path -DestinationPath "$extract_dir"
+  Expand-Archive -Path $download_path -DestinationPath "$extract_dir" -Force
 } else {
-  New-Item -ItemType Directory -Path "$extract_dir" -ErrorAction SilentlyContinue
   tar -xzf "$download_path" -C "$extract_dir"
   chmod +x "${download_path}${dirsep}${mpls}"
 }
 
-Move-Item "${extract_dir}${dirsep}${mpls}" "${HOME}${dirsep}.local${dirsep}bin${mpls}" -Force -ErrorAction SilentlyContinue
+Move-Item "${extract_dir}${dirsep}${mpls}" "${HOME}${dirsep}.local${dirsep}bin${dirsep}${mpls}" -Force
 
