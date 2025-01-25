c794b6103f10da81f945168249171930b4f05412
daniel <eduardo.daniel.s.m@gmail.com>
2025-01-25 11:21:58 -0500

    refactor(git): Move gitsigns into git directory

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -7,6 +7,12 @@ return {
     }
   },
   {
+    'lewis6991/gitsigns.nvim',
+    config = function ()
+      require('config.nvim_gitsigns').setup()
+    end
+  },
+  {
     'jecaro/fugitive-difftool.nvim',
     cmd = {
       -- To the first
--- lua/plugins/gitsigns.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'lewis6991/gitsigns.nvim',
-  config = function ()
-    require('config.nvim_gitsigns').setup()
-  end
-}
