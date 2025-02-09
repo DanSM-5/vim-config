6560140162653020e3aa2f9b61ab48b0eedbac25
daniel <eduardo.daniel.s.m@gmail.com>
2025-02-09 00:49:59 -0500

    feat(autopairs): Use correct dependencies for autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,8 +1,17 @@
+local autopairs_deps = os.getenv('USE_BLINK') == '1' and {} or {
+  {
+    'iguanacucumber/magazine.nvim',
+    -- 'hrsh7th/nvim-cmp', -- Currently substituted by magazine.nvim
+    -- NOTE: Using magazine.nvim as as nvim-cmp replacement
+    name = 'nvim-cmp',
+  },
+}
+
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   -- Optional dependency
-  dependencies = { 'hrsh7th/nvim-cmp' },
+  dependencies = autopairs_deps,
   config = function ()
     require('config.nvim_autopairs').setup()
   end,
