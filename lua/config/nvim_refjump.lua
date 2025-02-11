822c701aad5b58c40af484fa8048afd69d308f60
daniel <eduardo.daniel.s.m@gmail.com>
2025-02-10 23:02:05 -0500

    feat(refjump): Handle adding keys manually

--- lua/config/nvim_refjump.lua
+++ lua/config/nvim_refjump.lua
@@ -1,24 +1,76 @@
+local function repeatable_jump_map(opts)
+  local repeatably_do = require('demicolon.jump').repeatably_do
+  return function()
+    local references
+    repeatably_do(function(o)
+      require('refjump').reference_jump(o, references, function(refs)
+        references = refs
+      end)
+    end, opts)
+  end
+end
+
+---@param evt { buf: number, data?: { client_id?: number } }
+local function set_ref_key(evt)
+  ---@type vim.lsp.Client[]
+  local clients
+
+  -- if we get id, use it
+  if evt.data and evt.data.client_id then
+    clients = vim.lsp.get_clients({
+      id = evt.data.client_id,
+      method = 'textDocument/documentHighlight',
+    })
+  else
+    -- if not, fallback to buffer
+    clients = vim.lsp.get_clients({
+      bufnr = evt.buf,
+      method = 'textDocument/documentHighlight',
+    })
+  end
+
+  local supports_document_highlight = #clients > 0
+  if not supports_document_highlight then
+    return
+  end
+
+  local nxo = { 'n', 'x', 'o' }
+  local jump = repeatable_jump_map
+
+  vim.keymap.set(nxo, ']r', jump({ forward = true }), {
+    desc = 'Next reference',
+    buffer = evt.buf,
+  })
+
+  vim.keymap.set(nxo, '[r', jump({ forward = false }), {
+    desc = 'Previous reference',
+    buffer = evt.buf,
+  })
+
+  vim.keymap.set(nxo, '<f7>', jump({ forward = true }), {
+    desc = 'Next reference',
+    buffer = evt.buf,
+  })
+
+  vim.keymap.set(nxo, '<s-f7>', jump({ forward = false }), {
+    desc = 'Previous reference',
+    buffer = evt.buf,
+  })
+end
+
 return {
   setup = function ()
-    require('refjump').setup({})
+    require('refjump').setup({
+      keymaps = {
+        enable = false,
+      },
+    })
 
     -- Create extra mapping after change
     vim.api.nvim_create_autocmd('LspAttach', {
-      ---@param evt { buf: number, data: { client_id: number } }
-      callback = function(evt)
-        local client = vim.lsp.get_client_by_id(evt.data.client_id)
-        local supports_document_highlight = client and client.supports_method(
-          'textDocument/documentHighlight',
-          { bufnr = evt.buf }
-        )
-        if not supports_document_highlight then
-          return
-        end
-
-        vim.keymap.set({'o', 'x', 'n'}, '<f7>', ']r', { remap = true, desc = 'Next reference', buffer = evt.buf })
-        vim.keymap.set({'o', 'x', 'n'}, '<s-f7>', '[r', { remap = true, desc = 'Previous reference', buffer = evt.buf })
-      end,
+      callback = set_ref_key,
       desc = 'Add keymaps for refjump',
     })
-  end
+  end,
+  set_ref_key = set_ref_key,
 }
