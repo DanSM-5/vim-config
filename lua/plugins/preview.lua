9a82cd126de1bd808c640f0280fb07bc742a7725
daniel <eduardo.daniel.s.m@gmail.com>
2025-02-16 22:09:27 -0500

    refactor(neoimg): Lazy load neo-img

--- lua/plugins/preview.lua
+++ lua/plugins/preview.lua
@@ -2,7 +2,9 @@ return {
   'Skardyy/neo-img',
   -- Only load if we have the dependency installed
   enabled = vim.fn.executable('ttyimg') == 1,
-  event = 'VeryLazy',
+  -- event = 'VeryLazy',
+  ft = { 'oil' },
+  cmd = { 'NeoImg' },
   opts = {},
 }
 
