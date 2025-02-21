b8a42c55f6850b929a3eacd7d27387ef09073829
daniel <eduardo.daniel.s.m@gmail.com>
2025-02-21 18:44:08 -0500

    fix(textobjects): Disable quote pairs text objects for mini.ai
    The quotes `"`, `'` and "`" seem to get confused more on complex
    situations than the built-in text objects. E.g.
    
    ```bash
    ybb="$(git config --get-color "" "yellow| black bold")"
    ```
    
    Mini.ai is unable to find any text object in the sorrounding
    quotes as it seems to match the quote pairs from left to right
    which means the area from `yellow` to `bold` is not inside a quote.
    
    That line is tricky even for the built-in text objects but overall
    they seem to perform more reasonable.
    
    This disables the custom text objects for the 3 type of quotes in
    favor of the builtins.

--- lua/config/nvim_mai.lua
+++ lua/config/nvim_mai.lua
@@ -3,6 +3,11 @@ return {
     require('mini.ai').setup({
       search_method = 'cover',
       n_lines = 999999,
+      custom_textobjects = {
+        ["'"] = false,
+        ['"'] = false,
+        ['`'] = false,
+      },
     })
   end
 }
