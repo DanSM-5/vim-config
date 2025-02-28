5cffb8fe392108685bae8e8bfe09396e2497d807
daniel <eduardo.daniel.s.m@gmail.com>
2025-02-28 18:53:06 -0500

    refactor(mai): Asing function call to 'F' keymap

--- lua/config/nvim_mai.lua
+++ lua/config/nvim_mai.lua
@@ -1,13 +1,16 @@
 return {
   setup = function ()
-    require('mini.ai').setup({
+    local ai = require('mini.ai')
+    ai.setup({
       search_method = 'cover',
       n_lines = 999999,
       custom_textobjects = {
         ["'"] = false,
         ['"'] = false,
         ['`'] = false,
+        -- Set function call to F
         ['f'] = false,
+        ['F'] = ai.gen_spec.function_call(),
       },
     })
   end
