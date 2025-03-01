55a5ec45c0cbcfa5fd839667e56929ca4128be0e
daniel <eduardo.daniel.s.m@gmail.com>
2025-02-28 23:39:58 -0500

    test: Simplify neo-img

--- lua/plugins/preview.lua
+++ lua/plugins/preview.lua
@@ -1,10 +1,8 @@
 return {
   'Skardyy/neo-img',
   -- Only load if we have the dependency installed
-  enabled = vim.fn.executable('ttyimg') == 1,
-  -- event = 'VeryLazy',
-  ft = { 'oil' },
-  cmd = { 'NeoImg' },
-  opts = {},
+  config = function ()
+    require('neo-img').setup()
+  end
 }
 
