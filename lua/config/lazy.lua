eb86798a0944f58fe90860dcf0220c630bb51cb5
daniel <eduardo.daniel.s.m@gmail.com>
2025-02-28 23:41:22 -0500

    test: Add hints

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -26,6 +26,13 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     import = 'plugins',
+    -- NOTE: Comment above and uncomment below to only load neo-img and oil
+    -- {
+    --   import = 'plugins.preview',
+    -- },
+    -- {
+    --   import = 'plugins.explorer'
+    -- }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
--- lua/plugins/preview.lua
+++ lua/plugins/preview.lua
@@ -1,6 +1,5 @@
 return {
   'Skardyy/neo-img',
-  -- Only load if we have the dependency installed
   config = function ()
     require('neo-img').setup()
   end
