d76f0c74f86e8237cdb5813db47770eece8a80fc
daniel <eduardo.daniel.s.m@gmail.com>
2025-02-28 23:38:02 -0500

    feat(config.lazy): Add type annotations

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,9 +22,10 @@ local check_for_updates = not (uv.fs_stat(vim.fn.stdpath('config') .. '/.no_upda
 
 -- Setup lazy.nvim
 require("lazy").setup({
+  ---@type LazySpec
   spec = {
     -- import your plugins
-    { import = "plugins" },
+    import = 'plugins',
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
