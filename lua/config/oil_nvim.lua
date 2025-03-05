ddfe52936a1fdd49fea3fa8705880b347ba12f3b
daniel <eduardo.daniel.s.m@gmail.com>
2025-03-04 21:56:00 -0500

    refactor(oil): Add back netrw commands

--- lua/config/oil_nvim.lua
+++ lua/config/oil_nvim.lua
@@ -4,6 +4,22 @@ local search_dir = function()
   require('config.nvim_grugfar').open_from_explorer(prefills)
 end
 
+local set_commands = function ()
+
+  -- If we want to restore netrw commands (do I?)
+  -- Ref: https://github.com/saccarosium/netrw.vim/blob/fd09e094525c796f6220666246a11e9be258be69/plugin/netrwPlugin.vim
+  vim.cmd([[
+    command! -nargs=* -bar -bang -count=0 -complete=dir Explore call netrw#Explore(<count>, 0, 0+<bang>0, <q-args>)
+    command! -nargs=* -bar -bang -count=0 -complete=dir Sexplore call netrw#Explore(<count>, 1, 0+<bang>0, <q-args>)
+    command! -nargs=* -bar -bang -count=0 -complete=dir Hexplore call netrw#Explore(<count>, 1, 2+<bang>0, <q-args>)
+    command! -nargs=* -bar -bang -count=0 -complete=dir Vexplore call netrw#Explore(<count>, 1, 4+<bang>0, <q-args>)
+    command! -nargs=* -bar -count=0 -complete=dir Texplore call netrw#Explore(<count>, 0, 6, <q-args>)
+    command! -nargs=* -bar -bang -count=0 -complete=dir Lexplore call netrw#Lexplore(<count>, <bang>0, <q-args>)
+    command! -nargs=* -bar -bang Nexplore call netrw#Explore(-1, 0, 0, <q-args>)
+    command! -nargs=* -bar -bang Pexplore call netrw#Explore(-2, 0, 0, <q-args>)
+  ]])
+end
+
 return {
   setup = function()
     require('oil').setup({
@@ -19,5 +35,6 @@ return {
     vim.keymap.set('n', '<leader>-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
   end,
   search_dir = search_dir,
+  set_commands = set_commands,
 }
 
