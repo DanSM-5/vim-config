9bf7be66b0d0759c11f2d7a24d672f15a027175f
daniel <eduardo.daniel.s.m@gmail.com>
2025-03-14 17:13:56 -0400

    feat(gopls): Add configuration for gopls

--- lua/lsp-servers/config.lua
+++ lua/lsp-servers/config.lua
@@ -12,9 +12,10 @@ local configs = {
       Lua = {
         runtime = { version = 'LuaJIT' },
         telemetry = { enabled = false },
-        workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+        workspace = { library = vim.api.nvim_get_runtime_file('', true), checkThirdparty = false },
         format = {
-          -- enable = true,
+          enable = true,
+          insert_final_newline = true,
           defaultConfig = {
             insert_final_newline = true,
           },
@@ -59,7 +60,44 @@ local configs = {
         validate = { enable = true },
       },
     },
-  }
+  },
+  gopls = {
+    settings = {
+      gopls = {
+        gofumpt = true,
+        codelenses = {
+          gc_details = false,
+          generate = true,
+          regenerate_cgo = true,
+          run_govulncheck = true,
+          test = true,
+          tidy = true,
+          upgrade_dependency = true,
+          vendor = true,
+        },
+        hints = {
+          assignVariableTypes = true,
+          compositeLiteralFields = true,
+          compositeLiteralTypes = true,
+          constantValues = true,
+          functionTypeParameters = true,
+          parameterNames = true,
+          rangeVariableTypes = true,
+        },
+        analyses = {
+          nilness = true,
+          unusedparams = true,
+          unusedwrite = true,
+          useany = true,
+        },
+        usePlaceholders = true,
+        completeUnimported = true,
+        staticcheck = true,
+        directoryFilters = { '-.git', '-.vscode', '-.idea', '-.vscode-test', '-node_modules' },
+        semanticTokens = true,
+      },
+    },
+  },
 }
 
 return {
