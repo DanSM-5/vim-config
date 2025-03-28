85337fab7b1f74e4d7ff1e5f44038bcee3b2ab2a
daniel <eduardo.daniel.s.m@gmail.com>
2025-03-28 12:04:24 -0400

    feat(shared): Add module for shared configs

---
+++ lua/shared/configs.lua
@@ -0,0 +1,5 @@
+-- NOTE: Do NOT set. It is a mess with some windows like fzf floating window
+-- if vim.fn.exists('&winborder') == 1 then
+--   vim.o.winborder = 'rounded'
+-- end
+
--- lua/shared/nvim_load.lua
+++ lua/shared/nvim_load.lua
@@ -2,5 +2,6 @@ require('shared.autocmd')
 require('shared.commands')
 require('shared.big_files').setup()
 require('shared.maps')
+require('shared.configs')
 -- require('debugger').setup()
 
