e1fb780810ce47864ef01291e0f8d2cf18ff7e37
daniel <eduardo.daniel.s.m@gmail.com>
2025-03-28 22:40:59 -0400

    refactor(diagnostics): Update sign symbol definitions
    Old version with the use of `sign_define` is deprecated

--- lua/config/nvim_lspconfig.lua
+++ lua/config/nvim_lspconfig.lua
@@ -37,18 +37,18 @@ return {
 
     -- Signs for diagnostics
     local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
-    -- vim.diagnostic.config({
-    --   signs = { text = {
-    --     [vim.diagnostic.severity.ERROR] = signs.Error,
-    --     [vim.diagnostic.severity.WARN] = signs.Warn,
-    --     [vim.diagnostic.severity.HINT] = signs.Hint,
-    --     [vim.diagnostic.severity.INFO] = signs.Info,
-    --   } }
-    -- })
+		-- for type, icon in pairs(signs) do
+		-- 	local hl = "DiagnosticSign" .. type
+		-- 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		-- end
+    vim.diagnostic.config({
+      signs = { text = {
+        [vim.diagnostic.severity.ERROR] = signs.Error,
+        [vim.diagnostic.severity.WARN] = signs.Warn,
+        [vim.diagnostic.severity.HINT] = signs.Hint,
+        [vim.diagnostic.severity.INFO] = signs.Info,
+      } }
+    })
 
     -- Start diagnostics (virtual text) enabled
     vim.diagnostic.config({
