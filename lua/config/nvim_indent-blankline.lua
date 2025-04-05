1f614f62976968444be95fe6d884798a327e620f
daniel <eduardo.daniel.s.m@gmail.com>
2025-04-05 11:33:09 -0400

    fix(ibl): Reset used highlight colors

--- lua/config/nvim_indent-blankline.lua
+++ lua/config/nvim_indent-blankline.lua
@@ -1,5 +1,12 @@
 return {
   setup = function ()
+    local hooks = require('ibl.hooks')
+    -- create the highlight groups in the highlight setup hook, so they are reset
+    -- every time the colorscheme changes
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
+      vim.api.nvim_set_hl(0, 'RainbowGrey', { fg = '#3b4048' })
+    end)
     -- Configure to enable ibl only
     require('ibl').setup({
       indent = {
