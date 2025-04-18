be3349f51a7a50144575de4a28e47faaeb134c4b
daniel <eduardo.daniel.s.m@gmail.com>
2025-04-18 01:13:14 -0400

    fix(LuaSnip): Fix loading issue with lazy

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,10 +3,13 @@
 -- indicate wheter to use cmp or blink
 local use_blink = os.getenv('USE_BLINK') == '1'
 
+---@type LazyPluginSpec
 local luasnip_spec = {
   'L3MON4D3/LuaSnip',
+  lazy = true,
   -- follow latest release.
-  -- version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  -- tag = 'v2.*',
   -- install jsregexp (optional!).
   build = (function ()
     if vim.fn.executable('make') == 0 then
@@ -14,6 +17,45 @@ local luasnip_spec = {
     end
     return 'make install_jsregexp'
   end)(),
+}
+
+---@type LazyPluginSpec[]
+local blink_deps = use_blink and {
+  -- Snippets
+  'rafamadriz/friendly-snippets',
+  luasnip_spec,
+  {
+    'saghen/blink.compat',
+    -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+    version = '*',
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    -- opts = {},
+  },
+  'petertriho/cmp-git',
+  'roginfarrer/cmp-css-variables',
+  'mikavilpas/blink-ripgrep.nvim',
+} or {}
+
+---@type LazyPluginSpec[]
+local cmp_deps = use_blink and {} or {
+  -- Snippets
+  'rafamadriz/friendly-snippets',
+  luasnip_spec,
+  'saadparwaiz1/cmp_luasnip',
+  'petertriho/cmp-git',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-nvim-lsp-signature-help',
+  'roginfarrer/cmp-css-variables',
+  'lukas-reineke/cmp-rg',
+  'nvim-tree/nvim-web-devicons',
+  -- 'hrsh7th/cmp-nvim-lua' -- { name = 'nvim_lua'  }
+  -- 'hrsh7th/cmp-buffer' -- { name = 'path' }
+  -- 'https://codeberg.org/FelipeLema/cmp-async-path' -- { name = 'async_path' }
+  -- 'hrsh7th/cmp-path' -- { name = 'buffer' }
+  -- 'hrsh7th/cmp-cmdline' -- { name = 'cmd' }
+  -- 'Jezda1337/nvim-html-css' -- { name = 'html-css' }
 }
 
 -- Entry point of lsp related plugins
@@ -66,26 +108,10 @@ return {
     event = 'InsertEnter',
     -- optional: provides snippets for the snippet source
     -- dependencies = 'rafamadriz/friendly-snippets',
-    dependencies = {
-      -- Snippets
-      'rafamadriz/friendly-snippets',
-      luasnip_spec,
-      {
-        'saghen/blink.compat',
-        -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
-        version = '*',
-        -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
-        lazy = true,
-        -- make sure to set opts so that lazy.nvim calls blink.compat's setup
-        -- opts = {},
-      },
-      'petertriho/cmp-git',
-      'roginfarrer/cmp-css-variables',
-      'mikavilpas/blink-ripgrep.nvim',
-    },
+    dependencies = blink_deps,
 
     -- use a release tag to download pre-built binaries
-    version = 'v0.*',
+    version = 'v1.*',
     -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
     -- build = 'cargo build --release',
     -- If you use nix, you can build from source using latest nightly rust with:
@@ -107,24 +133,7 @@ return {
     enabled = not use_blink,
     event = 'InsertEnter',
     name = 'nvim-cmp',
-    dependencies = {
-      -- Snippets
-      'rafamadriz/friendly-snippets',
-      luasnip_spec,
-      'saadparwaiz1/cmp_luasnip',
-      'petertriho/cmp-git',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-      'roginfarrer/cmp-css-variables',
-      'lukas-reineke/cmp-rg',
-      'nvim-tree/nvim-web-devicons',
-      -- 'hrsh7th/cmp-nvim-lua' -- { name = 'nvim_lua'  }
-      -- 'hrsh7th/cmp-buffer' -- { name = 'path' }
-      -- 'https://codeberg.org/FelipeLema/cmp-async-path' -- { name = 'async_path' }
-      -- 'hrsh7th/cmp-path' -- { name = 'buffer' }
-      -- 'hrsh7th/cmp-cmdline' -- { name = 'cmd' }
-      -- 'Jezda1337/nvim-html-css' -- { name = 'html-css' }
-    },
+    dependencies = cmp_deps,
     config = function ()
       require('config.nvim_cmp').configure({ lazydev = true })
     end,
@@ -137,10 +146,6 @@ return {
       'williamboman/mason-lspconfig.nvim',
       -- Lsp for linters/formatters
       'nvimtools/none-ls.nvim',
-      -- Snippets
-      -- NOTE: Do not remove LuaSnip
-      'L3MON4D3/LuaSnip',
-      -- 'rafamadriz/friendly-snippets',
       -- Dependency
       'nvim-tree/nvim-web-devicons',
       -- Ctags lsp
