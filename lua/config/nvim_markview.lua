5e249e37394d7b88276531e6d9ebbd8b9bbd27c4
daniel <eduardo.daniel.s.m@gmail.com>
2025-06-24 18:22:53 -0400

    feat(markview): silence warning about loading treesitter

--- lua/config/nvim_markview.lua
+++ lua/config/nvim_markview.lua
@@ -1,13 +1,23 @@
+---@module 'markview'
+
 return {
   setup = function ()
     -- Toggle :Markview
-    require('markview').setup({
+
+    ---@type markview.config
+    local config = {
       preview = {
         enable = vim.g.is_termux == 1,
         icon_provider = 'internal',
       },
-    })
+      ---@diagnostic disable-next-line
+      experimental = {
+        check_rtp_message = false,
+      },
+    }
+
+    require('markview').setup(config)
     require('markview.extras.checkboxes').setup()
-  end
+  end,
 }
 
