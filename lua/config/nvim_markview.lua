30b50e42a5eadf42a0aaea6ec6f623ece75e6660
daniel <eduardo.daniel.s.m@gmail.com>
2025-11-11 01:01:12 -0500

    refactor: update markview config

--- lua/config/nvim_markview.lua
+++ lua/config/nvim_markview.lua
@@ -10,10 +10,6 @@ return {
         enable = vim.g.is_termux == 1,
         icon_provider = 'internal',
       },
-      ---@diagnostic disable-next-line
-      experimental = {
-        check_rtp_message = false,
-      },
     }
 
     require('markview').setup(config)
