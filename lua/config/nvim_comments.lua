814960adad54cdbf76fa60cde9703ad68dbadf6b
daniel <eduardo.daniel.s.m@gmail.com>
2026-02-14 17:42:27 -0500

    refactor(deprecated): remove deprecated call to vim.loader.disable

--- lua/config/nvim_comments.lua
+++ lua/config/nvim_comments.lua
@@ -1,18 +1,15 @@
 return {
-  setup = function ()
+  setup = function()
     require('ts_context_commentstring').setup({
       enable_autocmd = false,
     })
 
-    if vim.loader.disable ~= nil then
-      vim.loader.disable()
-    else
-      vim.loader.enable(false)
-    end
+    vim.loader.enable(false)
+    ---@diagnostic disable-next-line: missing-fields
     require('Comment').setup({
       pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
       ignore = '^$',
     })
     vim.loader.enable(true)
-  end
+  end,
 }
