03c8c5d96e95eb1645eeff885d4e9c4367128dbc
daniel <eduardo.daniel.s.m@gmail.com>
2026-03-28 13:31:19 -0400

    fix(autopairs): do not enable auto `()` on powershell

--- lua/config/nvim_autopairs.lua
+++ lua/config/nvim_autopairs.lua
@@ -12,6 +12,7 @@ return {
     autopairs.setup({
       -- ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
       -- enable_check_bracket_line = false,
+      -- disable_filetype
     })
 
     -- Prevent pair if previous is alphanumeric and `.`
@@ -23,7 +24,11 @@ return {
       -- If you want to automatically add `(` after selecting a function or method
       local cmp_autopairs = require('nvim-autopairs.completion.cmp')
       local cmp = require('cmp')
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({
+        filetypes = {
+          ps1 = false,
+        }
+      }))
     end
   end
 }
